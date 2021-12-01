7574e8f91c7789dab682f769088d7816732dc47d
jflogon646 <jflor@iesrarfaelalberti.es>
2021-12-01 17:40:31 +0100

    Listo

--- src/main/kotlin/Clases.kt
+++ src/main/kotlin/Clases.kt
@@ -26,40 +26,114 @@ data class Ciudad(val nombre: String) {
 
 data class Tienda(val nombre: String, val clientes: List<Cliente>) {
     //4.2.1
-    fun obtenerConjuntoDeClientes() {
-
+    fun obtenerConjuntoDeClientes(): Set<Cliente> {
+        return clientes.toSet()
     }
-    //4.2.2
-    fun obtenerCiudadesDeClientes() {} //set<ciudad>
-    fun obtenerClientesPorCiudad(ciudad: Ciudad) {} //list<cliente>
-    //4.2.3
-    fun checkTodosClientesSonDe(ciudad: Ciudad) {} //Boolean
-    fun hayClientesDe(ciudad: Ciudad) {} //Boolean
-    fun cuentaClientesDe(ciudad: Ciudad) {} //Int
-    fun encuentraClienteDe(ciudad: Ciudad) {} //Cliente?
-    //4.2.4
-    fun obtenerClientesOrdenadosPorPedidos() {} //List<Cliente>
-    //4.2.5
-    fun obtenerClientesConPedidosSinEntregar() {} //set<cliente>
-    //4.2.6
-    fun obtenerProductosPedidos() {} //Set<Producto>
-    //4.2.7
-    fun obtenerProductosPedidosPorTodos() {} //set<producto>
-    //4.2.8
-    fun obtenerNumeroVecesProductoPedido(producto: Producto) {} //producto?
-    //4.2.9
-    fun agrupaClientesPorCiudad() {} //map<ciudad,list<cliente>>
-    //4.2.10
+
+
+    //4.3.2
+    fun obtenerCiudadesDeClientes(): Set<Ciudad> {
+        var ciudades: MutableList<Ciudad> = mutableListOf(Ciudad(""))
+        for (cliente in clientes) {
+            ciudades.add(cliente.ciudad)
+        }
+        ciudades.removeFirst()
+        return ciudades.toSet()
+    }
+
+    fun obtenerClientesPorCiudad(ciudad: Ciudad): List<Cliente> {
+        return clientes.filter { it -> it.ciudad == ciudad }
+    }
+
+
+    //4.3.3
+    fun checkTodosClientesSonDe(ciudad: Ciudad): Boolean {
+        return (clientes.all { it -> it.ciudad == ciudad })
+    }
+
+    fun hayClientesDe(ciudad: Ciudad): Boolean {
+        return (clientes.any { it -> it.ciudad == ciudad })
+    }
+
+    fun cuentaClientesDe(ciudad: Ciudad): Int {
+        return clientes.filter { it -> it.ciudad == ciudad }.size
+    }
+
+    fun encuentraClienteDe(ciudad: Ciudad): Cliente {
+        return clientes[clientes.indexOfFirst { it -> it.ciudad == ciudad }]
+    }
+
+
+    //4.3.4
+    fun obtenerClientesOrdenadosPorPedidos(): List<Cliente> {
+        return clientes.sortedByDescending { it -> it.pedidos.size }
+    }
+
+
+    //4.3.5
+    fun obtenerClientesConPedidosSinEntregar(): Set<Cliente> {
+        return clientes.filter { it ->
+            it.pedidos.any { !it.estaEntregado }
+        }.toSet()
+    }
+
+
+    //4.3.6
+    fun obtenerProductosPedidos(): Set<Producto> {
+        val lista: MutableList<Producto> = mutableListOf<Producto>(Producto("",0.0))
+        for (cliente in clientes) {
+            cliente.pedidos.forEach { it -> it.productos.forEach { it -> lista.add(it) } }
+        }
+        lista.removeFirst()
+        return lista.toSet()
+    }
+
+
+    //4.3.7
+    fun obtenerProductosPedidosPorTodos(): Set<Producto> {
+        val lista: MutableList<Producto> = mutableListOf<Producto>(Producto("",0.0))
+        for (cliente in clientes) {
+            cliente.pedidos.forEach { it -> it.productos.forEach { it -> lista.add(it) } }
+        }
+        lista.removeFirst()
+        return lista.toSet()
+    }
+
+    //4.3.8
+    fun obtenerNumeroVecesProductoPedido(producto: Producto): Int {
+        var productoMasPedido: Producto
+        var contador = 0
+        for (cliente in clientes) {
+            contador += cliente.obtenerProductosPedidos().filter { it -> it.nombre == producto.nombre }.size
+        }
+        return contador
+    }
+
+    //4.3.9
+    fun agrupaClientesPorCiudad(): MutableMap<Ciudad, List<Cliente>> {
+        // Inicialización del map
+        var mapa: MutableMap<Ciudad,List<Cliente>> = mutableMapOf(Ciudad("") to listOf<Cliente>(Cliente("",Ciudad(""),
+            listOf(Pedido(listOf(Producto("",0.0)),true)))))
+
+        var lista: List<Cliente>
+        for (ciudad in obtenerCiudadesDeClientes()) {
+            mapa[ciudad] = obtenerClientesPorCiudad(ciudad)
+        }
+        mapa.remove(Ciudad(""))
+        return mapa
+    }
+
+
+    //4.3.10
     fun mapeaNombreACLiente() {} // map<String, Cliente>
     fun mapeaClienteACiudad() {} // map<Cliente, Ciudad>
     fun mapeaNombreClienteACiudad() {} // map<String, Ciudad>
-    //4.2.11
-    fun obtenerClientesConMasPedidos() {} //Cliente?
 
 
-    /*
-    Funciones auxiliares privadas
-     */
+    //4.3.11
+    fun obtenerClientesConMasPedidos(): Cliente? {
+        return clientes.maxByOrNull { it -> it.pedidos.size }
+    }
 
 }
 
@@ -69,13 +143,39 @@ data class Tienda(val nombre: String, val clientes: List<Cliente>) {
  */
 data class Cliente(val nombre: String, val ciudad: Ciudad, val pedidos: List<Pedido>) {
     override fun toString() = "$nombre from ${ciudad.nombre}"
-    //4.2.6
-    fun obtenerProductosPedidos() {} //list<producto>
-    //4.2.8
-    fun encuentraProductoMasCaro() {} // Producto?
-    //4.2.11
-    fun obtenerProductoMasCaroPedido() {} //Producto?
-    //4.2.12
-    fun dineroGastado() {} // Double
+
+
+    //4.3.6
+    fun obtenerProductosPedidos(): List<Producto> {
+        val lista: MutableList<Producto> = mutableListOf<Producto>(Producto("",0.0))
+        pedidos.forEach { it -> it.productos.forEach { it -> lista.add(it) } }
+        lista.removeFirst()
+        return lista
+
+    }
+
+
+    //4.3.8
+    fun encuentraProductoMasCaro(): Producto? {
+        val lista: MutableList<Producto> = mutableListOf(Producto("",0.0))
+        for (pedido in pedidos) {
+            if (pedido.estaEntregado) {
+                lista.add(pedido.productos.maxByOrNull { it -> it.precio }!!)
+            }
+        }
+        return lista.maxByOrNull { it -> it.precio }
+    }
+
+
+    //4.3.11
+    fun obtenerProductoMasCaroPedido(): Producto? {
+        return obtenerProductosPedidos().maxByOrNull { it -> it.precio }
+    }
+    //4.3.12
+    fun dineroGastado(): Double {
+        var contador = 0.0
+        obtenerProductosPedidos().forEach { it -> contador += it.precio }
+        return contador
+    }
 
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@
  * Clase ejecutable
  */
 fun main() {
+    //Objetos Ciudad
     var ciudad1: Ciudad = Ciudad("Madrid")
     var ciudad2: Ciudad = Ciudad("Cádiz")
     var ciudad3: Ciudad = Ciudad("Barcelona")
@@ -9,6 +10,8 @@ fun main() {
     var ciudad5: Ciudad = Ciudad("Sevilla")
     var ciudad6: Ciudad = Ciudad("Granada")
 
+
+    //Objetos Producto
     var producto1: Producto = Producto("Disco duro SSD Kingston 240GB",24.99)
     var producto2: Producto = Producto("Memoria RAM Corsair 16GB(2x8GB)",81.40)
     var producto3: Producto = Producto("MSI Modern 14 i5",698.99)
@@ -22,6 +25,7 @@ fun main() {
     var producto11: Producto = Producto("MSI Katana GF66",1049.50)
     var producto12: Producto = Producto("Asus Rog Strix G15",2099.90)
 
+    //Objetos Pedido
     var pedido1: Pedido = Pedido(listOf(producto1,producto4,producto4),true)
     var pedido2: Pedido = Pedido(listOf(producto3,producto6,producto11),true)
     var pedido3: Pedido = Pedido(listOf(producto12,producto4,producto4,producto11),false)
@@ -39,18 +43,75 @@ fun main() {
     var pedido15: Pedido = Pedido(listOf(producto2,producto4,producto8),true)
 
 
+    //Objetos Cliente
     var cliente1: Cliente = Cliente("Antonio Fernández",ciudad1, listOf(pedido2,pedido3,pedido10))
     var cliente2: Cliente = Cliente("Jesús Gutiérrez",ciudad3,listOf(pedido1))
-    var cliente3: Cliente = Cliente("Alejandro Benítez",ciudad2,listOf(pedido5))
-    var cliente4: Cliente = Cliente("Dorian Leal",ciudad4,listOf(pedido15,pedido14,pedido6,pedido4))
+    var cliente3: Cliente = Cliente("Alejandro Benítez",ciudad2,listOf(pedido14))
+    var cliente4: Cliente = Cliente("Dorian Leal",ciudad4,listOf(pedido15,pedido5,pedido6,pedido4))
     var cliente5: Cliente = Cliente("Paolo Galindo",ciudad4,listOf(pedido12,pedido7))
     var cliente6: Cliente = Cliente("Manuel Revuelta",ciudad3,listOf(pedido8))
     var cliente7: Cliente = Cliente("Agustín González",ciudad5,listOf(pedido9,pedido11))
     var cliente8: Cliente = Cliente("Cristóbal Tamayo",ciudad6,listOf(pedido13))
 
-    val tienda: Tienda = Tienda("Pccomponentes",
-        listOf(cliente1,cliente2,cliente3,cliente4,cliente5,cliente6,cliente7,cliente8))
+    //Objeto Tienda
+    val tienda: Tienda = Tienda("Pccomponentes", listOf(cliente1,cliente2,cliente3,cliente4,cliente5,cliente6,cliente7,cliente8))
 
 
+    
+    //4.3.1
+    println("*".repeat(15) + " Apartado 4.3.1 " + "*".repeat(15))
+    println(tienda.obtenerConjuntoDeClientes())
 
+    //4.3.2
+    println("*".repeat(15) + " Apartado 4.3.2 " + "*".repeat(15))
+    println(tienda.obtenerCiudadesDeClientes())
+    println(tienda.obtenerClientesPorCiudad(ciudad4))
+
+    //4.3.3
+    println("*".repeat(15) + " Apartado 4.3.3 " + "*".repeat(15))
+    println(tienda.checkTodosClientesSonDe(ciudad4))
+    println(tienda.hayClientesDe(ciudad4))
+    println(tienda.cuentaClientesDe(ciudad4))
+    println(tienda.encuentraClienteDe(ciudad4))
+
+    //4.3.4
+    println("*".repeat(15) + " Apartado 4.3.4 " + "*".repeat(15))
+    println(tienda.obtenerClientesOrdenadosPorPedidos())
+
+    //4.3.5
+    println("*".repeat(15) + " Apartado 4.3.5 " + "*".repeat(15))
+    println(tienda.obtenerClientesConPedidosSinEntregar())
+
+    //4.3.6
+    println("*".repeat(15) + " Apartado 4.3.6 " + "*".repeat(15))
+    println(tienda.obtenerProductosPedidos())
+    println(cliente4.obtenerProductosPedidos())
+
+    //4.3.7
+    println("*".repeat(15) + " Apartado 4.3.7 " + "*".repeat(15))
+    println(tienda.obtenerProductosPedidosPorTodos())
+
+    //4.3.8
+    println("*".repeat(15) + " Apartado 4.3.8 " + "*".repeat(15))
+    println(cliente1.encuentraProductoMasCaro())
+    println(tienda.obtenerNumeroVecesProductoPedido(producto4))
+
+    //4.3.9
+    println("*".repeat(15) + " Apartado 4.3.9 " + "*".repeat(15))
+    println(tienda.agrupaClientesPorCiudad())
+
+    //4.3.10
+    println("*".repeat(15) + " Apartado 4.3.10 " + "*".repeat(15))
+    println()
+    println()
+    println()
+
+    //4.3.11
+    println("*".repeat(15) + " Apartado 4.3.11 " + "*".repeat(15))
+    println(tienda.obtenerClientesConMasPedidos())
+    println(cliente1.obtenerProductoMasCaroPedido())
+
+    //4.3.12
+    println("*".repeat(15) + " Apartado 4.3.12 " + "*".repeat(15))
+    println(cliente4.dineroGastado())
 }
